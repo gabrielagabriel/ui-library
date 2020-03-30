81127f25680e2b35cef328a2a4c4920066b22961
Gabriela Gabriel <gabrielagabriel@gmail.com>
2020-03-30 11:19:24 +0200

    [BBC-6997] Styled components on index ::nails::

--- src/successModal/SuccessModal.tsx
+++ src/successModal/SuccessModal.tsx
@@ -1,81 +1,10 @@
 import React from 'react'
 import uuidv4 from 'uuid/v4'
 
-import styled from 'styled-components'
+import { ModalProps } from 'modal'
+import { ButtonStatus } from 'button'
 
-import { color, space, responsiveBreakpoints } from '_utils/branding'
-
-import IllustratedSection from 'layout/section/illustratedSection'
-
-import Modal, { ModalProps } from 'modal'
-import Button, { ButtonStatus } from 'button'
-import TextDisplay1 from 'typography/display1'
-
-// Override Modal: Success
-const StyledSuccessModal = styled(Modal)`
-  padding: 0;
-  text-align: center;
-  background-color: ${color.successBackground};
-
-  .kirk-modal-dialog {
-    display: flex;
-    justify-content: center;
-    padding: 0;
-    margin: 0 auto;
-    width: 100%;
-    border-radius: 0;
-    background-color: ${color.successBackground};
-  }
-
-  .kirk-modal-body {
-    display: flex;
-    min-height: 100vh;
-    flex-direction: column;
-
-    @media (${responsiveBreakpoints.isMediaLarge}) {
-      flex-direction: row;
-      align-items: center;
-      flex-wrap: wrap;
-    }
-  }
-`
-
-// Override IllustratedSection: Centering vertically
-const StyledIllustratedSection = styled(IllustratedSection)`
-  display: flex;
-  flex: 1;
-
-  .kirk-illustratedSection-illustration {
-    padding-top: calc(2 * ${space.xxl});
-    justify-content: center;
-    align-items: center;
-
-    @media (${responsiveBreakpoints.isMediaLarge}) {
-      padding-top: 0;
-    }
-  }
-
-  .kirk-illustratedSection-content {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-  }
-`
-
-// Apply layout and spacements to Text
-const StyledText = styled(TextDisplay1)`
-  padding: ${space.xl};
-  flex: 1;
-`
-
-// Override Button
-const StyledButton = styled(Button)`
-  margin: ${space.xl};
-`
-
-const Action = styled.div`
-  padding: ${space.xl};
-`
+import { SuccessModalStyle } from './index'
 
 export interface SuccessModalProps extends ModalProps {
   readonly confirmLabel?: string
@@ -97,6 +26,14 @@ const SuccessModal = (props: SuccessModalProps): JSX.Element => {
 
   const successContentId = `kirk-successModal-content-${uuidv4()}`
 
+  const {
+    StyledSuccessModal,
+    StyledIllustratedSection,
+    SuccessTitle,
+    SuccessAction,
+    SuccessButton,
+  } = SuccessModalStyle
+
   return (
     <StyledSuccessModal
       onClose={onClose}
@@ -109,16 +46,16 @@ const SuccessModal = (props: SuccessModalProps): JSX.Element => {
       ariaLabelledBy={successContentId}
     >
       <StyledIllustratedSection illustrationUrl={imageSrc} illustrationAlt={imageText}>
-        <StyledText isInverted>{children}</StyledText>
-        <Action>
-          <StyledButton
+        <SuccessTitle isInverted>{children}</SuccessTitle>
+        <SuccessAction>
+          <SuccessButton
             status={ButtonStatus.SECONDARY}
             data-test="success-button"
             onClick={onClose}
           >
             {confirmLabel}
-          </StyledButton>
-        </Action>
+          </SuccessButton>
+        </SuccessAction>
       </StyledIllustratedSection>
     </StyledSuccessModal>
   )
--- src/successModal/index.tsx
+++ src/successModal/index.tsx
@@ -1,4 +1,84 @@
+import styled from 'styled-components'
 import SuccessModal from './SuccessModal'
 
+import { color, space, responsiveBreakpoints } from '_utils/branding'
+
+import IllustratedSection from 'layout/section/illustratedSection'
+
+import Modal from 'modal'
+import Button from 'button'
+import TextDisplay1 from 'typography/display1'
+
+const StyledSuccessModal = styled(Modal)`
+  padding: 0;
+  text-align: center;
+  background-color: ${color.successBackground};
+
+  .kirk-modal-dialog {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    margin: 0 auto;
+    width: 100%;
+    border-radius: 0;
+    background-color: ${color.successBackground};
+  }
+
+  .kirk-modal-body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+
+    @media (${responsiveBreakpoints.isMediaLarge}) {
+      flex-direction: row;
+      align-items: center;
+      flex-wrap: wrap;
+    }
+  }
+`
+
+const StyledIllustratedSection = styled(IllustratedSection)`
+  display: flex;
+  flex: 1;
+
+  .kirk-illustratedSection-illustration {
+    padding-top: calc(2 * ${space.xxl});
+    justify-content: center;
+    align-items: center;
+
+    @media (${responsiveBreakpoints.isMediaLarge}) {
+      padding-top: 0;
+    }
+  }
+
+  .kirk-illustratedSection-content {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+`
+const SuccessTitle = styled(TextDisplay1)`
+  padding: ${space.xl};
+  flex: 1;
+`
+
+const SuccessAction = styled.div`
+  padding: ${space.xl};
+`
+
+// @todo: This should be applyed on Button component since it is part of its specification
+const SuccessButton = styled(Button)`
+  margin: ${space.xl};
+`
+
+const SuccessModalStyle = {
+  StyledSuccessModal,
+  StyledIllustratedSection,
+  SuccessTitle,
+  SuccessAction,
+  SuccessButton,
+}
+
 export { SuccessModalProps } from './SuccessModal'
+export { SuccessModalStyle }
 export default SuccessModal
